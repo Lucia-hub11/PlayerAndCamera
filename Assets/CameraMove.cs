67a2710a83a90ee3faf094c3140a1347dd565ad4
Lucia-hub11 <lucia.macias@estudiantat.upc.edu>
2024-10-23 09:55:54 +0200

    cam

--- Assets/CameraMove.cs
+++ Assets/CameraMove.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float TurnSpeed = 1;
+    InputController _input;
+
     void Start()
     {
-        
+        _input = GetComponent<InputController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Turn();
+    }
+
+    private void Turn()
+    {
+        var localInput = transform.right * _input.Look.x + transform.forward * _input.Look.y;
+        Vector3 direction = new Vector3(localInput.x, 0, localInput.z);
+        if (direction.magnitude > 0)
+        {
+            Vector3 target = transform.position + direction;
+            Vector3 current = transform.position + transform.forward;
+            Vector3 look = Vector3.Lerp(current, target, TurnSpeed * Time.deltaTime);
+            transform.LookAt(look);
+        }
     }
 }
